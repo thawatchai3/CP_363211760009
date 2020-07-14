2469378498bd4849979193f39ecc2590aeb35f56
Thawatchai <thawatchai.mu@rmutsvmail.com>
2020-07-14 17:55:15 +0700

    add code 2 line

--- src/Lab1/Helloworld.java
+++ src/Lab1/Helloworld.java
@@ -4,8 +4,11 @@ public class Helloworld {
 
     public static void main(String[] args) {
         System.out.println("Hello World !!!");
-        System.out.println("Thawatchai Muadthip !!!");
-
+        System.out.println("Thawatchai Muadthip");
+        System.out.println("NickName mos");
+        System.out.println("Age 22");
+        System.out.println("High Garden 170");
+        System.out.println("Telphon 0620519990!!!");
     }
 
 
