eed779e5ebebad48fab77d2b8659f3b7f7bfdf2a
Thawatchai <thawatchai.mu@rmutsvmail.com>
2020-07-14 17:41:37 +0700

    Initial commit

---
+++ src/Lab1/Helloworld.java
@@ -0,0 +1,13 @@
+package Lab1;
+
+public class Helloworld {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World !!!");
+        System.out.println("Thawatchai Muadthip !!!");
+
+    }
+
+
+
+}
